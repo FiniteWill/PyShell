68a76f03bdf17bd8d15e25636d7408acaa99d1d2
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2023-02-01 02:11:23 -0600

    Added ASCII code image on startup

--- PyShell.py
+++ PyShell.py
@@ -4,6 +4,13 @@ running = True
 
 if __name__ == "__main__":
     print("Starting PyShell.")
+    print( "      ________  \n"
+          +"       _______\n"
+          +"       |    O|\n"
+          +"  ____ |  ___|-<\n"
+          +" |  __||  |\n"
+          +" |  |__|  |\n"
+          +" |________|")
     while running:
         command = input("Enter a command or type 'help' for help. \n")
         ps.parse(command)
